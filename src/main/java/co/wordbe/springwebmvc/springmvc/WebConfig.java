e31a651d61aeac0d222e5763056a556f9a1cf819
Wordbe <seonghojin3@gmail.com>
2021-02-12 23:10:31 +0900

    210212 resource handler

--- src/main/java/co/wordbe/springwebmvc/springmvc/WebConfig.java
+++ src/main/java/co/wordbe/springwebmvc/springmvc/WebConfig.java
@@ -1,15 +1,30 @@
 package co.wordbe.springwebmvc.springmvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new GreetingInterceptor());
-        registry.addInterceptor(new AnotherInterceptor());
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/mvc")
+                .order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+                .resourceChain(true)
+        ;
     }
 }
--- src/test/java/co/wordbe/springwebmvc/springmvc/MvcControllerTest.java
+++ src/test/java/co/wordbe/springwebmvc/springmvc/MvcControllerTest.java
@@ -1,15 +1,16 @@
 package co.wordbe.springwebmvc.springmvc;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@WebMvcTest
 @SpringBootTest
@@ -33,4 +34,16 @@ class MvcControllerTest {
                 .andDo(print())
                 .andExpect(content().string("mvc, 1"));
     }
+
+    @Test
+    public void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL))
+                ;
+    }
+
+
 }
